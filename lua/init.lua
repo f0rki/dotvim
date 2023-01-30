ca312cee2e0ee13966a759421c3fe3629140e613
Michael Rodler <mrodler@amazon.de>
2023-01-30 20:28:27 +0000

    some recommended lsp keybindings

--- lua/init.lua
+++ lua/init.lua
@@ -117,11 +117,59 @@ require("lspconfig").grammar_guard.setup({
 })
 --]]
 
+
+------ lsp keybindings
+-- https://github.com/neovim/nvim-lspconfig#Suggested-configuration
+
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
+local lsp_flags = {
+  -- This is the default in Nvim 0.7+
+  debounce_text_changes = 150,
+}
+
+local lsp_std_config = {
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+
 -- initialize the lsps
-require("lspconfig").rust_analyzer.setup({})
-require("lspconfig").clangd.setup({})
-require("lspconfig").pylsp.setup({})
-require("lspconfig").gopls.setup({})
+require("lspconfig").rust_analyzer.setup(lsp_std_config)
+require("lspconfig").clangd.setup(lsp_std_config)
+require("lspconfig").pylsp.setup(lsp_std_config)
+require("lspconfig").gopls.setup(lsp_std_config)
+
 -- we want a bunch of nice rusty features :)
 require("rust-tools").setup({})
 
