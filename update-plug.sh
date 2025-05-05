de28db91989a602202e00b10703e7fd7adecf3f2
Michael Rodler <m@mrodler.eu>
2025-05-05 14:31:28 +0200

    added update script

---
+++ update-plug.sh
@@ -0,0 +1,5 @@
+nvim --headless -c 'PlugUpgrade --sync' -c qa
+nvim --headless -c 'PlugUpdate --sync' -c qa
+nvim --headless -c UpdateRemotePlugins -c qa
+nvim --headless -c 'TSInstallSync! rust c cpp python lua bash' -c qa
+
