a40dd766920805f41ed8feb367f5aeef9ee4e679
Michael Rodler <michael.rodler@cologne-intelligence.de>
2026-02-25 16:07:39 +0100

    remove minuet from completions

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -104,7 +104,7 @@ return {
             -- Default list of enabled providers defined so that you can extend it
             -- elsewhere in your config, without redefining it, due to `opts_extend`
             sources = {
-                default = { "lsp", "path", "snippets", "buffer", "emoji", "minuet" },
+                default = { "lsp", "path", "snippets", "buffer", "emoji" },
 
                 providers = {
                     -- minuet = {
